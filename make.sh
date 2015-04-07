df5d899f6dd229900ed43c1861e1ba514b0714fd
Joseph Dean <joe@on-site.com>
2015-04-07 06:17:15 -0700

    Automatically update sumodules when running make.sh script

--- make.sh
+++ make.sh
@@ -29,3 +29,8 @@ for file in $files; do
     echo "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/.$file
 done
+
+echo "Updating submodules"
+git submodule init
+git submodule update --recursive
+echo "Submodules updated"
